83bbb98bd75513df209ccbf4a70b301055fdf8db
Amar Aryal <amararyal878@gmail.com>
2024-02-19 18:26:02 +0545

    setup filter event and change event from enum to class

--- lib/blocs/characters_bloc/characters_bloc.dart
+++ lib/blocs/characters_bloc/characters_bloc.dart
@@ -1,22 +1,48 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rapid_recall/blocs/characters_bloc/characters_event.dart';
 import 'package:rapid_recall/blocs/characters_bloc/characters_state.dart';
+import 'package:rapid_recall/data/models/character.dart';
 import 'package:rapid_recall/data/repository/data_repository.dart';
 
 class CharactersBloc extends Bloc<CharactersEvent, CharactersState> {
   final DataRepository _dataRepository;
 
+  List<Character> _characters = [];
+
   CharactersBloc(this._dataRepository) : super(CharactersLoadingState()) {
     on<CharactersEvent>((event, emit) async {
-      emit(CharactersLoadingState());
-
-      try {
-        final characters = await _dataRepository.getCharactersData(1);
-
-        emit(CharactersLoadedState(characters));
-      } catch (e) {
-        emit(CharactersErrorState(e.toString()));
+      if (event is GetCharactersEvent) {
+        await getAllCharacters(event, emit);
+      } else if (event is FilterCharactersEvent) {
+        filterCharacters(event, emit, event.filters);
       }
     });
+  }
+
+  Future getAllCharacters(
+      CharactersEvent event, Emitter<CharactersState> emit) async {
+    emit(CharactersLoadingState());
+
+    try {
+      _characters = await _dataRepository.getCharactersData(1);
+
+      emit(CharactersLoadedState(_characters));
+    } catch (e) {
+      emit(CharactersErrorState(e.toString()));
+    }
+  }
+
+  filterCharacters(CharactersEvent event, Emitter<CharactersState> emit,
+      (String, int) filters) {
+    try {
+      final filteredCharacters =
+          _characters.where((element) => element.pinyin.startsWith(filters.$1));
+
+      _characters = [...filteredCharacters];
+
+      emit(CharactersLoadedState(_characters));
+    } catch (e) {
+      emit(CharactersErrorState(e.toString()));
+    }
   }
 }
--- lib/blocs/characters_bloc/characters_event.dart
+++ lib/blocs/characters_bloc/characters_event.dart
@@ -1,2 +1,18 @@
-// convert to classes if use case requires it
-enum CharactersEvent { getCharactersEvent }
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+abstract class CharactersEvent extends Equatable {}
+
+class GetCharactersEvent extends CharactersEvent {
+  @override
+  List<Object> get props => [];
+}
+
+class FilterCharactersEvent extends CharactersEvent {
+  final (String, int) filters;
+
+  FilterCharactersEvent(this.filters);
+  @override
+  List<Object> get props => [filters];
+}
