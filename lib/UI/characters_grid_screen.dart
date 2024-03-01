c76410b613020d046d1eb79724ed81cd660a79da
Amar Aryal <amararyal878@gmail.com>
2024-03-01 21:19:45 +0545

    add dropdown for filter: in progress

--- lib/UI/characters_grid_screen.dart
+++ lib/UI/characters_grid_screen.dart
@@ -6,19 +6,38 @@ import 'package:rapid_recall/blocs/characters_bloc/characters_event.dart';
 import 'package:rapid_recall/blocs/characters_bloc/characters_state.dart';
 import 'package:rapid_recall/data/repository/data_repository.dart';
 
-class CharacterGridScreen extends StatelessWidget {
+class CharacterGridScreen extends StatefulWidget {
   const CharacterGridScreen({super.key});
+
+  @override
+  State<CharacterGridScreen> createState() => _CharacterGridScreenState();
+}
+
+class _CharacterGridScreenState extends State<CharacterGridScreen> {
+  final alphabetsList =
+      List.generate(26, (index) => String.fromCharCode(index + 65));
+
+  late String selectedAlphabet;
+
+  @override
+  void initState() {
+    super.initState();
+
+    selectedAlphabet = alphabetsList[0];
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) =>
           CharactersBloc(RepositoryProvider.of<DataRepository>(context))
-            ..add(CharactersEvent.getCharactersEvent),
+            ..add(GetCharactersEvent(hskNo: 3)),
       child: Scaffold(
         appBar: AppBar(),
         body: BlocBuilder<CharactersBloc, CharactersState>(
           builder: (context, state) {
+            final charactersBloc = BlocProvider.of<CharactersBloc>(context);
+
             if (state is CharactersLoadingState) {
               return const Center(
                 child: CircularProgressIndicator.adaptive(),
@@ -32,6 +51,50 @@ class CharacterGridScreen extends StatelessWidget {
 
               return CustomScrollView(
                 slivers: [
+                  SliverToBoxAdapter(
+                    child: Row(
+                      children: [
+                        DropdownButton(
+                          value:
+                              selectedAlphabet, // TRY:SELECT LOGIC IN BLOC CLASS
+                          items: alphabetsList
+                              .map(
+                                (e) => DropdownMenuItem<String>(
+                                  value: e,
+                                  child: Text(e),
+                                ),
+                              )
+                              .toList(),
+                          onChanged: (String? value) {
+                            if (value != null) {
+                              selectedAlphabet = value;
+                              charactersBloc.add(
+                                FilterCharactersEvent(
+                                  (value.toLowerCase(), 3),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                        MaterialButton(
+                          child: const Text('TEST FILTER'),
+                          onPressed: () {
+                            charactersBloc.add(
+                              FilterCharactersEvent(
+                                ('a', 1),
+                              ),
+                            );
+                          },
+                        ),
+                        MaterialButton(
+                          child: const Text('RESET'),
+                          onPressed: () {
+                            charactersBloc.add(GetCharactersEvent());
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                   SliverGrid(
                     gridDelegate:
                         const SliverGridDelegateWithMaxCrossAxisExtent(
