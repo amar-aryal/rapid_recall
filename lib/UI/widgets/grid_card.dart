a5f0ef745072eee9de77e46b2e2d10c8f6153cac
Amar Aryal <amararyal878@gmail.com>
2024-03-03 18:22:29 +0545

    display popove on card tap

--- lib/UI/widgets/grid_card.dart
+++ lib/UI/widgets/grid_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:popover/popover.dart';
 import 'package:rapid_recall/data/models/character.dart';
+import 'package:rapid_recall/utils/context_extension.dart';
 
 class GridCard extends StatelessWidget {
   final Character character;
@@ -8,12 +10,53 @@ class GridCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // USE THEMES
-    return SizedBox.square(
-      // dimension: MediaQuery.of(context).size.width * 0.05,
-      child: Card(
-        child: Text(
-          character.hanzi,
-          style: Theme.of(context).textTheme.displayMedium,
+    return GestureDetector(
+      onTap: () {
+        showPopover(
+          context: context,
+          bodyBuilder: (context) => SizedBox(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Text(
+                      character.hanzi,
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      character.pinyin,
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    const SizedBox(height: 15),
+                    ...character.translations.map(
+                      (e) => Text(
+                        e,
+                        style: Theme.of(context).textTheme.bodyLarge,
+                        // textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          direction: PopoverDirection.top,
+          height: context.height * 0.35,
+          width: context.width * 0.6,
+          arrowHeight: 15,
+          arrowWidth: 30,
+          radius: 15,
+        );
+      },
+      child: SizedBox.square(
+        // dimension: MediaQuery.of(context).size.width * 0.05,
+        child: Card(
+          child: Text(
+            character.hanzi,
+            style: Theme.of(context).textTheme.displayMedium,
+          ),
         ),
       ),
     );
